bcf98f15f58e067fcb15b9586d4fb87fa04564c3
Markus <mfau@itu.dk>
2021-09-09 17:49:17 +0200

    interface for isLeapyear and Implemented error handling

--- HelloWorld.Tests/UnitTest1.cs
+++ HelloWorld.Tests/UnitTest1.cs
@@ -15,11 +15,26 @@ namespace HelloWorld.Tests
 
             // Act
             // Program.(Metoden man vil teste)
-            
+
 
             // Assert
             // Assert.Equal("expectedOutput!", actualOutput);
             //Assert.Equal("Hello World!", output);
+        }
+
+        [Fact]
+        public void HelloWorld_Prints_Hello_World()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            // Act
+            Program.HelloWorld();
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+            // Assert
+            Assert.Equal("Hello World!", output);
         }
 
         [Fact]
@@ -36,19 +51,68 @@ namespace HelloWorld.Tests
             Assert.False(isLeapYear);
         }
 
-        [Fact]
-        public void Main_prints_Hello_World()
+        [Theory]
+        [InlineData("1600", "Yay")]
+        [InlineData("1584", "Yay")]
+        [InlineData("2000", "Yay")]
+        public void Prints_Yay_If_Leap_Year(string input, string output)
         {
-            // Arrange
+            //Arrange
             var writer = new StringWriter();
             Console.SetOut(writer);
 
-            // Act
-            Program.HelloWorld();
-            var output = writer.GetStringBuilder().ToString().Trim();
+            var reader = new StringReader(input);
+            Console.SetIn(reader);
 
-            // Assert
-            Assert.Equal("Hello World!", output);
+            //Act           
+            Program.IsLeapYearUI();
+            var actual = writer.GetStringBuilder().ToString().Trim();
+
+            //Assert
+            Assert.Equal(output, actual);
+        }
+
+        [Theory]
+        [InlineData("1803", "Nay")]
+        [InlineData("1907", "Nay")]
+        [InlineData("10003", "Nay")]
+        public void Prints_Nay_If_Not_Leap_Year(string input, string output)
+        {
+            //Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var reader = new StringReader(input);
+            Console.SetIn(reader);
+
+            //Act           
+            Program.IsLeapYearUI();
+            var actual = writer.GetStringBuilder().ToString().Trim();
+
+            //Assert
+            Assert.Equal(output, actual);
+        }
+    
+
+    [Theory]
+        [InlineData("1581", "Too old")]
+        [InlineData("0", "Too old")]
+        [InlineData("-1", "Too old")]
+        public void Prints_Too_Old_If_Not_Leap_Year(string input, string output)
+        {
+            //Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var reader = new StringReader(input);
+            Console.SetIn(reader);
+
+            //Act           
+            Program.IsLeapYearUI();
+            var actual = writer.GetStringBuilder().ToString().Trim();
+
+            //Assert
+            Assert.Equal(output, actual);
         }
     }
 }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,23 +6,56 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            // Navnet på metoden der skal køres
-            //HelloWorld();
-            //IsLeapYear();
+            // Metoden der skal køres
+            IsLeapYearUI();
         }
 
         public static void HelloWorld(){
             Console.WriteLine("Hello World!");
         }
 
-        public static bool IsLeapYear(int year){
-            if (year % 4 == 0 && year % 100 != 0){
-                return true;
-            } else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0){
-                return true;
-            } else {
-                return false;
+        public static void IsLeapYearUI(){
+            // Breaks tests since I can't figure out how to make the writer skip to next line.
+            //Console.WriteLine("Type in year to check if it is a leap year:");
+            
+            try {
+                var input = Console.ReadLine();
+                int year = Convert.ToInt32(input);
+                
+                if (IsLeapYear(year)){
+                    Console.WriteLine("Yay");
+                } else {
+                    Console.WriteLine("Nay");
+                }
             }
+
+            catch (FormatException){
+                Console.WriteLine("You can only input numbers, pal");
+            }
+
+            catch (ArgumentOutOfRangeException){
+                Console.WriteLine("Too old");
+            }
+
+            // Fanger alle exceptions som ikke er fanget endnu
+            catch (Exception e){
+                System.Console.WriteLine(e.Message);
+            }
+        }
+
+        public static bool IsLeapYear(int year)
+        {
+            if (year < 1583) {
+                throw new ArgumentOutOfRangeException();
+            }
+        
+           if (year % 4 == 0 && year % 100 != 0){
+                    return true;
+                } else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0){
+                    return true;
+                } else {
+                    return false;
+                }     
         }
     }
 }
